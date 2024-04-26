cc3bb31661ed95672d94ed9a2e7152929c156e45
paniJc <codepanijc@gmail.com>
2024-04-26 11:41:10 -0500

    feat: Add the type_hints file

---
+++ type_hints.py
@@ -0,0 +1,7 @@
+### Type hints ###
+# Al cambiar el tipo con el hint (pista o sugerencia), el programa trata de sugerirnos, basado en el tipo de datos, sus métodos #
+
+my_variable: str = 'This is a string variable'
+print(my_variable)
+print(type(my_variable))
+
